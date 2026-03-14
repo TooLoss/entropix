4b0df18ce4c9b4fd28dccbdfd49b05fc309de00b
Loss <bilele.elhaddadi@gmail.com>
2026-03-14 16:48:35 +0100

    feat: SDL3 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include "core/World.hpp"
 #include "render/TerminalRender.hpp"
 
@@ -7,17 +9,38 @@ int main(int argc, char *argv[]) {
     std::unique_ptr<World> world = std::make_unique<World>(SIZE);
     std::unique_ptr<TerminalRender> render = std::make_unique<TerminalRender>(*world);
 
-    Pixel sand = world->create_pixel_id(CellID::SAND);
-    world->set_pixel({1, 1}, sand);
-    render->print();
-    world->refresh();
-    render->print();
-    world->refresh();
-    world->refresh();
-    world->refresh();
-    world->refresh();
-    world->refresh();
-    render->print();
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (!SDL_CreateWindowAndRenderer("SDL3 Window", 800, 600, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return 1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255); // Dark background
+        SDL_RenderClear(renderer);
+
+        SDL_FRect rect = { 350, 250, 100, 100 };
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
     return 0;
 }
