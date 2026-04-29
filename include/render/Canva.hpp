e713194b7fd062e8858ffe5b96ce1895df0c5597
Bilele <bilele.elhaddadi@gmail.com>
2026-04-29 18:05:00 +0200

    feat: OnClick event for canvas

--- include/render/Canva.hpp
+++ include/render/Canva.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "utils/Coord.hpp"
+#include <functional>
 #include <SDL3/SDL_events.h>
 
 class GameState;
@@ -8,6 +9,8 @@ class Canva {
 private:
     Coord pos;
     Coord size;
+
+    std::function<void()> click_event;
 
 public:
     Canva(Coord pos, Coord size) : pos(pos), size(size) {}
@@ -24,4 +27,7 @@ public:
 
     virtual void refresh_canva();
     virtual void mouse_action(SDL_Event* event, GameState& gamestate);
+
+    void set_click_event(std::function<void()> func);
+    void event_clicked();
 };
