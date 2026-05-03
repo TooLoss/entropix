137b695e234c996aacb0c9cc7d2b43ee3e5eaaf9
Loss <bilele.elhaddadi@gmail.com>
2026-05-03 15:22:18 +0200

    fix: cell concrete

--- include/cells/CellConcrete.hpp
+++ include/cells/CellConcrete.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include "cells/Cell.hpp"
 
 class CellConcrete : public Cell {
 public:
     CellConcrete() : Cell({ 150, 145, 134, 255 }) {};
 
     virtual void update(Coord pos, World& world) const override;
-}
+};
